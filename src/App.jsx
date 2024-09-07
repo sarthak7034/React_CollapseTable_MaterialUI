df3f86a22c7cfe54fbb53ba8634b32c1e8fc3d6e
srgonroll <sarthak_660027@yahoo.co.in>
2024-09-07 16:01:10 +0530

    Table with collapse feature working

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+// import PropTypes from "prop-types";
+import TableRow from "@mui/material/TableRow";
+// import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import data from "../data.json";
+
+export default function CollapsibleTable() {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>ID</TableCell>
+            <TableCell align="center">Node_ID</TableCell>
+            <TableCell align="right">Numbers&nbsp;</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((e, index) => (
+            <>
+              <TableRow key={index}>
+                <TableCell>
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => setOpen(!open)}
+                  >
+                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                  </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {e.id}
+                </TableCell>
+                <TableCell>{e.node_id}</TableCell>
+                <TableCell>{e.number}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  style={{ paddingBottom: 0, paddingTop: 0 }}
+                  colSpan={6}
+                >
+                  <Collapse in={open} timeout="auto" unmountOnExit>
+                    <Box sx={{ margin: 1 }}>
+                      <Table size="small" aria-label="purchases">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Login</TableCell>
+                            <TableCell>ID</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {data.map((e, index) => (
+                            <TableRow key={index}>
+                              <TableCell component="th" scope="row">
+                                {e.user.login}
+                              </TableCell>
+                              <TableCell>{e.user.id}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </Box>
+                  </Collapse>
+                </TableCell>
+              </TableRow>
+            </>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
